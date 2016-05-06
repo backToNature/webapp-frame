1ecca756f62f417d7f369e5961da8952e1788ebd
郭豪(技术中心) <haoguo@sohu-inc.com>
2016-05-06 18:58:03 +0800

    update

--- src/extensions/bottom-tap-1/bottom-tap-1.js
+++ src/extensions/bottom-tap-1/bottom-tap-1.js
@@ -1,4 +1,6 @@
 $(function () {
     var api = window.api;
+    window.extensions['bottom-tap-1'] = {};
+    window.extensions['bottom-tap-1'].dom = $('<p>我是bottom-tap-1</p>');
     
 });
--- src/main.js
+++ src/main.js
@@ -1,3 +1,29 @@
+var util = {
+    loadJs: function (src, fun) {
+        var head = document.getElementsByTagName('head')[0] || document.head || document.documentElement;
+        var script = document.createElement('script');
+        script.setAttribute('type', 'text/javascript');
+        script.setAttribute('charset', 'UTF-8');
+        script.setAttribute('src', src);
+        if (typeof fun === 'function') {
+            if (window.attachEvent) {
+                script.onreadystatechange = function () {
+                    var r = script.readyState;
+                    if (r === 'loaded' || r === 'complete') {
+                        script.onreadystatechange = null;
+                        fun();
+                    }
+                };
+            } else {
+                script.onload = fun;
+                script.onerror = fun;
+            }
+        }
+        head.appendChild(script);
+    }
+};
+
+window.debug = true;
 
 window.tmpFn = [];
 if (!window.ready) {
@@ -6,6 +32,15 @@ if (!window.ready) {
     };
 }
 
+if (debug) {
+    util.loadJs('./lib/seajs/sea-1.2.0.js', function () {
+        seajs.use('./core/core.js');
+    });
+} else {
+    util.loadJs('./core/core.js');
+}
+
 window.ready(function () {
-    window.api.util.loadJs('./extensions/bottom-tap-1/bottom-tap-1.js');
+    var api = window.api;
+    api.router('pagesA', [], 'A');
 });
