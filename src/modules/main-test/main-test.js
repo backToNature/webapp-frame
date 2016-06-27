0d46b193b52306cf6c8e619499223a26ead8db88
郭豪(技术中心) <haoguo@sohu-inc.com>
2016-06-27 11:43:11 +0800

    update

--- src/modules/main-test/main-test.js
+++ src/modules/main-test/main-test.js
@@ -1,10 +1,13 @@
 define(function(require, exports, module) {
     'use strict';
     window.mdevApp.exports = function (data, $dom, api, modules) {
+
         $dom.find('.jump-2').on('tap', function () {
-            api.router.go('demo-level-2');
+            api.router.go('demo-level-2', {data: 123});
         });
+
         $dom.find('#wrapper').height($(window).height() - $('.module-footer-test').height());
+
         var myScroll = new IScroll('#wrapper', {
             scrollbars: true,
             mouseWheel: false,
